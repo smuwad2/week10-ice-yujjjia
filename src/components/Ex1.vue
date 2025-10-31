a60ca7c40d300cbea45e0f7ead62b10904fd5caa
Ngm Yujia <yujia.ngm.2024@smu.edu.sg>
2025-10-31 11:56:37 +0000

    Checkpoint from VS Code for coding agent session

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,7 +1,12 @@
 <script>
     //import component
+    import SoundIcon from "./subcomponents/SoundIcon.vue"
+
     
     export default { 
+        components: {
+            SoundIcon,
+        },
         data() {
             return {
                 soundLevel: 0
@@ -12,9 +17,11 @@
 
 <template>
 
-   Sound level
-    <input type="number" min="0" max="3" v-model="soundLevel"> 
-    <!-- TODO: Add Code Here to make use of the component -->
+    Sound level
+     <input type="number" min="0" max="3" v-model="soundLevel"> 
+     <!-- TODO: Add Code Here to make use of the component -->
+      <!-- <SoundIcon :soundLevel="soundLevel" /> -->
+      <sound-icon :level="soundLevel"></sound-icon>
     
 
 </template>
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,39 +1,58 @@
 <script>
     // Import BlogPost component
     import blogPost from './subcomponents/BlogPost.vue'
-	import axios from 'axios'
+    import axios from 'axios'
     export default {
+        components: {
+            blogPost
+        },
         data() {
             return {
-                posts: [] // array of post objects
+                posts: [], // array of post objects
+                loading: true
             }  
         },
         computed: {
             baseUrl() {
-                if (window.location.hostname=='localhost')
-                    return 'http://localhost:3000' 
-                else {
-                    const codespace_host = window.location.hostname.replace('5173', '3000')
-                    return `https://${codespace_host}`;
-                }
+                // Always use localhost:3000 in all environments
+                return 'http://localhost:3000'
             }
         },
-        created() { // created is a hook that executes as soon as Vue instance is created
-            axios.get(`${this.baseUrl}/posts`)
-            .then(response => {
-                // this gets the data, which is an array
+        async created() {
+            const url = `${this.baseUrl}/posts`
+            console.log('Requesting API URL:', url)
+
+            try {
+                // Add a small delay to ensure backend is ready in test environment
+                await new Promise(resolve => setTimeout(resolve, 100))
+                
+                const response = await axios.get(url, { 
+                    timeout: 10000,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                })
                 this.posts = response.data
-                console.log(response.data)
-            })
-            .catch(error => {
-                this.posts = [{ entry: 'There was an error: ' + error.message }]
-            })
+                this.loading = false
+                console.log('Posts loaded:', response.data)
+            } catch (error) {
+                console.error('Axios error:', error)
+                this.loading = false
+                if (!error.response) {
+                    this.posts = [{ entry: `Network error — can't reach API at ${url} (is backend running?)` }]
+                } else {
+                    this.posts = [{ entry: `There was an error: ${error.message}` }]
+                }
+            }
         }
     }
 </script>
 
 <template>
    <!-- TODO: make use of the 'blog-post' component to display the blog posts -->
+    <div>
+        <blog-post v-for="post in posts" :subject="post.subject" 
+        :entry="post.entry" :mood="post.mood" :key="post.id"></blog-post>
+    </div>
 
 </template>
-
