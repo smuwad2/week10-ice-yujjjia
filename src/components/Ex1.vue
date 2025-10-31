ebf58710e0be2a7190458de581eb78a2d7f1ddbd
Ngm Yujia <yujia.ngm.2024@smu.edu.sg>
2025-10-31 12:37:20 +0000

    updated ex1,2,3,4

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -21,7 +21,7 @@
      <input type="number" min="0" max="3" v-model.number="soundLevel"> 
      <!-- TODO: Add Code Here to make use of the component -->
       <SoundIcon :soundLevel="soundLevel" />
-    
+     
 
 </template>
 
